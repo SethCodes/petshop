a1a6b4c28dc1aad3007606603da3884c499df404
Seth Stephens <sethastephens8@gmail.com>
2021-07-22 15:11:18 -0400

    style updates to nav foot and sections

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ console.log(year);
 
 
 document.getElementById('copyright').innerHTML = `Copyright ${year}. All rights reserved.`
+
+//about page
+let rightRow = document.getElementById('pic-block-left');
+
+if(window.innerWidth < 990){
+    rightRow.classList.remove('col-8');
+    
+    
+} else {
+    rightRow.classList.add('col-8');
+}
+console.log(rightRow.classList);
